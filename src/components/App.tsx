2166122291a2734637555820a40ced27c677c50c
m-hodges <50115036+m-hodges@users.noreply.github.com>
2020-01-11 22:25:54 +1300

    remove bootstrap test code

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,9 @@
 import React from 'react'
 import { hot } from 'react-hot-loader'
 
-import Button from 'react-bootstrap/Button'
-
 const App = () => {
     return (
         <>
-            <Button />
         </>
     )
 }

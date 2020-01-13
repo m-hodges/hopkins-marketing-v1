091008e9296778f5642d0fb058a30f4800b59333
m-hodges <50115036+m-hodges@users.noreply.github.com>
2020-01-13 22:45:38 +1300

    begin work on footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,40 @@
 import React from 'react';
 
-import { Navbar } from 'react-bootstrap'
+import { Navbar, Nav, Container, Row, Col } from 'react-bootstrap'
+import { NavLink } from 'react-router-dom'
 
 
 const Footer = () => {
     return (
-        <Navbar bg='light' fixed='bottom'>
-            Footer
+        <Navbar bg='light' fixed='bottom' className='footer'>
+            <Container>
+            <Row>
+                <Col></Col>
+                <Col>
+                    <NavLink to='/home'>
+                        Home
+                    </NavLink>
+                </Col>
+                <Col>
+                    <Nav>
+                        <Nav.Item>
+                            <Nav.Link>
+                                <NavLink to='/home'>
+                                    Home
+                                </NavLink>
+                            </Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link>
+                                <NavLink to='/home'>
+                                    Contact
+                                </NavLink>
+                            </Nav.Link>
+                        </Nav.Item>
+                    </Nav>
+                </Col>
+            </Row>
+            </Container>
         </Navbar>
     );
 };

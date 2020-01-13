5a9a9c9f095d49dbfebae7721425f912106ccab2
m-hodges <50115036+m-hodges@users.noreply.github.com>
2020-01-13 22:57:20 +1300

    add social media icons

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,31 +3,33 @@ import React from 'react';
 import { Navbar, Nav, Container, Row, Col } from 'react-bootstrap'
 import { NavLink } from 'react-router-dom'
 import FacebookIcon from '@material-ui/icons/Facebook'
-
+import TwitterIcon from '@material-ui/icons/Twitter'
+import InstagramIcon from '@material-ui/icons/Instagram'
 
 const Footer = () => {
     return (
         <Navbar bg='dark' fixed='bottom' className='footer'>
-            <Container>
-            <Row>
-                <Col>
-                    <FacebookIcon />
-                </Col>
-                <Col>
-                    <NavLink to='/home'>
-                        Home
-                    </NavLink>
-                </Col>
-                <Col>
-                    <NavLink to='/home'>
-                        Home
-                    </NavLink>
-        
-                    <NavLink to='/home'>
-                        Contact
-                    </NavLink>        
-                </Col>
-            </Row>
+            <Container fluid>
+                <Row>
+                    <Col>
+                        <FacebookIcon />
+                        <TwitterIcon />
+                        <InstagramIcon />
+                    </Col>
+                    <Col>
+                        <NavLink to='/home'>
+                            Home
+                        </NavLink>
+                    </Col>
+                    <Col>
+                        <NavLink to='/home'>
+                            Home
+                        </NavLink>
+                        <NavLink to='/home'>
+                            Contact
+                        </NavLink>        
+                    </Col>
+                </Row>
             </Container>
         </Navbar>
     );

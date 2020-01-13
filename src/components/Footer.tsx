72b821378cef0ba3f64f8572e4e47e854c16bcf2
m-hodges <50115036+m-hodges@users.noreply.github.com>
2020-01-13 23:44:38 +1300

    further work on footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Navbar, Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col } from 'react-bootstrap'
 import { NavLink } from 'react-router-dom'
 import FacebookIcon from '@material-ui/icons/Facebook'
 import TwitterIcon from '@material-ui/icons/Twitter'
@@ -8,34 +8,39 @@ import InstagramIcon from '@material-ui/icons/Instagram'
 
 const Footer = () => {
     return (
-        <Navbar bg='dark' fixed='bottom' className='footer'>
-            <Container fluid className='footer--container'>
-                <Row className='footer--container__row'>
-                    <Col sm={12} md={4} className='footer--container__column'>
-                        <img src="/images/hopkins_marketing_logo.jpg" alt="Hopkins Marketing Logo" className='footer--image' />
-                    </Col>
-                    <Col sm={6} md={4} className='footer--container__column'>
-                        <a href="#" target="_blank" rel="noopener noreferrer" className='footer--link'>
-                            <FacebookIcon className='footer--icon'/>
-                        </a>  
-                        <a href="#" target="_blank" rel="noopener noreferrer" className='footer--link'>
-                            <TwitterIcon className='footer--icon'/>
-                        </a>
-                        <a href="#" target="_blank" rel="noopener noreferrer" className='footer--link'>
-                            <InstagramIcon className='footer--icon'/>
-                        </a>
-                    </Col>
-                    <Col sm={6} md={4} className='footer--container__column'>
-                        <NavLink to='/home' className='footer--link'>
-                            Home
-                        </NavLink>
-                        <NavLink to='/contact' className='footer--link'>
-                            Contact Us
-                        </NavLink>
-                    </Col>
-                </Row>
-            </Container>
-        </Navbar>
+        <Container fluid className='footer'>
+            <Row className='footer--row__main'>
+                <Col sm={6} className='footer--column footer--column__centered'>
+                    <img src="/images/hopkins_marketing_logo.jpg" alt="Hopkins Marketing Logo" className='footer--image' />
+                </Col>
+                <Col sm={3} className='footer--column'>
+                    <a href="#" target="_blank" rel="noopener noreferrer" className='footer--link'>
+                        <FacebookIcon className='footer--icon'/>
+                    </a>  
+                    <a href="#" target="_blank" rel="noopener noreferrer" className='footer--link'>
+                        <TwitterIcon className='footer--icon'/>
+                    </a>
+                    <a href="#" target="_blank" rel="noopener noreferrer" className='footer--link'>
+                        <InstagramIcon className='footer--icon'/>
+                    </a>
+                </Col>
+                <Col sm={3} className='footer--column'>
+                    <NavLink to='/home' className='footer--link'>
+                        Home
+                    </NavLink>
+                    <NavLink to='/contact' className='footer--link'>
+                        Contact Us
+                    </NavLink>
+                </Col>
+            </Row>
+            <Row className='footer--row__sub'>
+                <Col className='footer--column footer--column__centered'>
+                    <span className='border-top'>
+                        Copyright © 2020 Hopkins Marketing Group | All Rights Reserved.
+                    </span>
+                </Col>
+            </Row>
+        </Container>
     );
 };
 

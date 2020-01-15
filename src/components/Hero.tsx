702b6b91da4f6878cb02a71d8313cb1414c62f88
m-hodges <50115036+m-hodges@users.noreply.github.com>
2020-01-16 04:30:40 +1300

    changes to hero page

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,8 +3,15 @@ import React from 'react';
 const Hero = () => {
     return (
         <div className='hero container--centered'>
-            <h1>H</h1>
-            <h1>Hopkins Marketing Group</h1>
+            <div className='hero--background'></div>
+            <div>
+                <h1 className='hero--logo'>H</h1>
+            </div>
+            <div className='hero--titleContainer'>
+                <h2 className='hero--title'>Hopkins</h2>
+                <h2 className='hero--title'>Marketing</h2>
+                <h2 className='hero--title'>Group</h2>
+            </div>
         </div>
     );
 };

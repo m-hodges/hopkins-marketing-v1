7428508c915947d2e4d24c6c839ee4f2cb45e971
m-hodges <50115036+m-hodges@users.noreply.github.com>
2020-01-16 04:05:46 +1300

    navbar styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,13 +5,13 @@ const Header = () => {
     return (
         <>
             <div className='header'>
-                <div className=''>Hopkins Marketing Group</div>
-                <div className='header--links'>
-                    <NavLink to='/home' activeClassName='activeNavLink'>Home</NavLink>
-                    <NavLink to='/about' activeClassName='activeNavLink'>About</NavLink>
-                    <NavLink to='/services' activeClassName='activeNavLink'>Services</NavLink>
-                    <NavLink to='/clients' activeClassName='activeNavLink'>Clients</NavLink>
-                    <NavLink to='/contact' activeClassName='activeNavLink'>Contact</NavLink>
+                <div className='header--brand'>Hopkins Marketing Group</div>
+                <div className='header--linkContainer'>
+                    <NavLink to='/home' activeClassName='activeNavLink' className='header--link'>Home</NavLink>
+                    <NavLink to='/about' activeClassName='activeNavLink' className='header--link'>About</NavLink>
+                    <NavLink to='/services' activeClassName='activeNavLink' className='header--link'>Services</NavLink>
+                    <NavLink to='/clients' activeClassName='activeNavLink' className='header--link'>Clients</NavLink>
+                    <NavLink to='/contact' activeClassName='activeNavLink' className='header--link'>Contact</NavLink>
                 </div>
             </div>
         </>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 const Hero = () => {
     return (
         <div className='hero container--centered'>
+            <h1>H</h1>
             <h1>Hopkins Marketing Group</h1>
         </div>
     );

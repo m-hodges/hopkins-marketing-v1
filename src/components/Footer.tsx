d52ec43f49b39ab04e5b69174c7cb9b91a4169a7
m-hodges <50115036+m-hodges@users.noreply.github.com>
2020-01-16 03:23:12 +1300

    completely remove all bootstrap

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,17 @@
 import React from 'react';
 
-import { Container, Row, Col } from 'react-bootstrap'
 import FacebookIcon from '@material-ui/icons/Facebook'
 import TwitterIcon from '@material-ui/icons/Twitter'
 import InstagramIcon from '@material-ui/icons/Instagram'
 
 const Footer = () => {
     return (
-        <Container fluid className='footer'>
-            <Row className='footer--row__main'>
-                <Col sm={6} className='footer--column footer--column__centered'>
+        <div className='footer'>
+            <div className='footer--row__main'>
+                <div className='footer--column footer--column__centered'>
                     <img src="/images/hopkins_marketing_logo.jpg" alt="Hopkins Marketing Logo" className='footer--image' />
-                </Col>
-                <Col sm={3} className='footer--column'>
+                </div>
+                <div className='footer--column'>
                     <a href="#" target="_blank" rel="noopener noreferrer" className='footer--link'>
                         <FacebookIcon className='footer--icon'/>
                     </a>  
@@ -22,21 +21,21 @@ const Footer = () => {
                     <a href="#" target="_blank" rel="noopener noreferrer" className='footer--link'>
                         <InstagramIcon className='footer--icon'/>
                     </a>
-                </Col>
-                <Col sm={3} className='footer--column'>
+                </div>
+                <div className='footer--column'>
                     <p>Contact Details:</p>
                     <p>Ph: 027312313123</p>
                     <p>Email: hopkinsmarketing@gmail.com</p>
-                </Col>
-            </Row>
-            <Row className='footer--row__sub'>
-                <Col className='footer--column footer--column__centered'>
+                </div>
+            </div>
+            <div className='footer--row__sub'>
+                <div className='footer--column footer--column__centered'>
                     <span className='border-top'>
                         Copyright © 2020 Hopkins Marketing Group | All Rights Reserved.
                     </span>
-                </Col>
-            </Row>
-        </Container>
+                </div>
+            </div>
+        </div>
     );
 };
 

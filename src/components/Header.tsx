fe095ff8f327f1d8221b0a5d22f3d8d14ac2be6f
m-hodges <50115036+m-hodges@users.noreply.github.com>
2020-01-16 03:44:26 +1300

    remove all skeleton

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,15 +5,13 @@ const Header = () => {
     return (
         <>
             <div className='header'>
-                <div className="row">
-                    <div className='one-third column'>Hopkins Marketing Group</div>
-                    <div className='header--links two-thirds column'>
-                        <NavLink to='/home' activeClassName='activeNavLink'>Home</NavLink>
-                        <NavLink to='/about' activeClassName='activeNavLink'>About</NavLink>
-                        <NavLink to='/services' activeClassName='activeNavLink'>Services</NavLink>
-                        <NavLink to='/clients' activeClassName='activeNavLink'>Clients</NavLink>
-                        <NavLink to='/contact' activeClassName='activeNavLink'>Contact</NavLink>
-                    </div>
+                <div className=''>Hopkins Marketing Group</div>
+                <div className='header--links'>
+                    <NavLink to='/home' activeClassName='activeNavLink'>Home</NavLink>
+                    <NavLink to='/about' activeClassName='activeNavLink'>About</NavLink>
+                    <NavLink to='/services' activeClassName='activeNavLink'>Services</NavLink>
+                    <NavLink to='/clients' activeClassName='activeNavLink'>Clients</NavLink>
+                    <NavLink to='/contact' activeClassName='activeNavLink'>Contact</NavLink>
                 </div>
             </div>
         </>

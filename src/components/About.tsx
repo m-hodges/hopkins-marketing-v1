22148792e68cae7c6f9220da3f718e6317ad2b64
m-hodges <50115036+m-hodges@users.noreply.github.com>
2020-01-25 00:56:12 +1300

    readjust styles for about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div className='about container--half'>
-            <h2>We're here to help</h2>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris aliquet lectus dapibus tristique volutpat. Maecenas pretium ipsum non ullamcorper elementum. Duis eu iaculis metus. Pellentesque at ligula euismod, feugiat nisi in, sollicitudin turpis.
+        <div className='about container--centered__col'>
+            <h2>
+                Lorem Ipsum
+                <hr/>
+            </h2>
+            <p className='about--content'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris aliquet lectus dapibus tristique volutpat. Maecenas pretium ipsum non ullamcorper elementum. Duis eu iaculis metus. Pellentesque at ligula euismod, feugiat nisi in, sollicitudin turpis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris aliquet lectus dapibus tristique volutpat. Maecenas pretium ipsum non ullamcorper elementum. Duis eu iaculis metus. Pellentesque at ligula euismod, feugiat nisi in, sollicitudin turpis.
             </p>
         </div>
     );
--- src/data.ts
+++ src/data.ts
@@ -11,6 +11,22 @@ const data: IService[] = [
     {
         title: 'Website Creation',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris aliquet lectus dapibus tristique volutpat. Maecenas pretium ipsum non ullamcorper elementum. Duis eu iaculis metus. Pellentesque at ligula euismod, feugiat nisi in, sollicitudin turpis. Mauris lacinia velit nisi, in rutrum augue congue vel.'
+    },
+    {
+        title: 'Website Creation',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris aliquet lectus dapibus tristique volutpat. Maecenas pretium ipsum non ullamcorper elementum. Duis eu iaculis metus. Pellentesque at ligula euismod, feugiat nisi in, sollicitudin turpis. Mauris lacinia velit nisi, in rutrum augue congue vel.'
+    },
+    {
+        title: 'Website Creation',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris aliquet lectus dapibus tristique volutpat. Maecenas pretium ipsum non ullamcorper elementum. Duis eu iaculis metus. Pellentesque at ligula euismod, feugiat nisi in, sollicitudin turpis. Mauris lacinia velit nisi, in rutrum augue congue vel.'
+    },
+    {
+        title: 'Website Creation',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris aliquet lectus dapibus tristique volutpat. Maecenas pretium ipsum non ullamcorper elementum. Duis eu iaculis metus. Pellentesque at ligula euismod, feugiat nisi in, sollicitudin turpis. Mauris lacinia velit nisi, in rutrum augue congue vel.'
+    },
+    {
+        title: 'Website Creation',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris aliquet lectus dapibus tristique volutpat. Maecenas pretium ipsum non ullamcorper elementum. Duis eu iaculis metus. Pellentesque at ligula euismod, feugiat nisi in, sollicitudin turpis. Mauris lacinia velit nisi, in rutrum augue congue vel.'
     }
 ]
 

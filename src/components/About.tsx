4efb3949db5fbf10c1180aeb149a3e54cb20f551
m-hodges <50115036+m-hodges@users.noreply.github.com>
2020-01-25 01:45:10 +1300

    remove bootstrap dep

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from 'react';
 
 const About = () => {
     const [ isContentVisible, setContentVisibility ] = useState<boolean>(false)
+    const [ isHrVisible, setHrVisibility ] = useState<boolean>(false)
     useEffect(() => {
         const observer = new IntersectionObserver((entries) => {
             entries.forEach(entry => {
+                setHrVisibility(entry.isIntersecting)
                 if (entry.isIntersecting) {
                     setContentVisibility(true)
                 }
@@ -18,7 +20,7 @@ const About = () => {
         <div className='about container--centered__col' >
             <h2 id='contentReference'>
                 Lorem Ipsum
-                <hr className={`about--hr ${(isContentVisible) ? 'about--hr__visible' : 'about--hr__not-visible'}`}/>
+                <hr className={`about--hr ${(isHrVisible) ? 'about--hr__visible' : 'about--hr__not-visible'}`}/>
             </h2>
             <p className={`about--content ${(isContentVisible) ? 'fade-in fade-in--visible' : 'fade-in'}`}>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris aliquet lectus dapibus tristique volutpat. Maecenas pretium ipsum non ullamcorper elementum. Duis eu iaculis metus. Pellentesque at ligula euismod, feugiat nisi in, sollicitudin turpis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris aliquet lectus dapibus tristique volutpat. Maecenas pretium ipsum non ullamcorper elementum. Duis eu iaculis metus. Pellentesque at ligula euismod, feugiat nisi in, sollicitudin turpis.

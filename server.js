05e109ae9febdb5395d063535fd4d760b4b91d53
m-hodges <50115036+m-hodges@users.noreply.github.com>
2020-03-11 20:37:10 +1300

    get cors working so can send requests from client to proxy server

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const server = express();
 const router = require("./routes");
 
+server.use(cors());
 server.use(bodyParser.json());
 server.use(express.static(__dirname));
 server.use(express.static(path.join(__dirname, "public")));

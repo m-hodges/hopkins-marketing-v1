f891212ed6d732a8a5bc3d43f10eb31fa9a5a47d
m-hodges <50115036+m-hodges@users.noreply.github.com>
2020-03-12 21:26:01 +1300

    cleanup

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -11,21 +11,6 @@ type Props = {
 };
 
 const Main = ({ setHeaderVisibility }: Props) => {
-  // async function getApi() {
-  //   const response = await fetch("https://cat-fact.herokuapp.com/facts", {
-  //     method: "GET",
-  //     mode: "cors",
-  //     credentials: "omit",
-  //     //   crossdomain: true,
-  //     headers: {
-  //       "Access-Control-Allow-Origin": "*"
-  //     }
-  //   });
-  //   return await response.json();
-  // }
-  // const nyaa = getApi();
-  // console.log(nyaa);
-
   return (
     <>
       <Hero setHeaderVisibility={setHeaderVisibility} />
